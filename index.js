0a86756574f9fe252418e5d7d0dacb3a2233e1f7
Vikrant Yadav <vikrantmani45@gmail.com>
2024-03-10 11:15:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+//first method
+// import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+// const freecurrencyapi = new Freecurrencyapi('fca_live_1vfg4nsg15emIowoVeJ34KVtRKw0KelVu3OxvqHp');
+
+// convertCurrency("INR", "USD", 3);
+
+// export async function convertCurrency(fromCurrency, toCurrency, units) {
+//   const res = await freecurrencyapi.latest({
+//     base_currency: fromCurrency,
+//     currencies: toCurrency
+//   });
+//   const multiplier = res.data[toCurrency];
+//   return multiplier * units;
+//   //console.log(multiplier);
+// }
+
+
+
+//second method if you are finding any error while testing
 import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 // Create a new instance of Freecurrencyapi
